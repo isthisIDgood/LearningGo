a8ede011c0f5ce95efcdc0454923293ea4958df7
isthisIDgood <34673447+isthisIDgood@users.noreply.github.com>
2018-01-01 01:36:25 -0500

    adds 02 and 03 files

---
+++ 02_packages/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/isthisIDgood/LearningGo/02_packages/stringutil"
+)
+
+func main(){
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+}

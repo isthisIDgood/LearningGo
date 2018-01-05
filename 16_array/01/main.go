017464bc6fa5afa7ca27510f3063d6048e017cfd
isthisIDgood <34673447+isthisIDgood@users.noreply.github.com>
2018-01-04 23:56:39 -0500

    adds a lot more files

--- 16_array/01/main.go
+++ 16_array/01/main.go
@@ -2,12 +2,12 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	var x [58]string
 	fmt.Println(x)
 	fmt.Println(len(x))
 	fmt.Println(x[42])
-	for i := 65; i <= 122; i++{
+	for i := 65; i <= 122; i++ {
 		x[i-65] = string(i)
 	}
 	fmt.Println(x)
--- 16_array/02/main.go
+++ 16_array/02/main.go
@@ -2,12 +2,12 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	var x [256]int
-	for i :=0; i < 256; i++{
+	for i := 0; i < 256; i++ {
 		x[i] = i
 	}
-	for _, v := range x{
+	for _, v := range x {
 		fmt.Printf("%v - %T - %b\n", v, v, v)
-		}
 	}
+}

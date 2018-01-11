87204fb5b4cf2c7561adf172c6e170565fa5b9ad
isthisIDgood <34673447+isthisIDgood@users.noreply.github.com>
2018-01-11 14:49:44 -0500

    adds comments to 02

--- 21.5_Hands-on-Exercises/02_slice-string/02/main.go
+++ 21.5_Hands-on-Exercises/02_slice-string/02/main.go
@@ -7,6 +7,9 @@ import (
 
 func main() {
 	s := []string{"Zeno", "John", "Al", "Jenny"}
+	//Reverses type StringSlice and returns and interface which gets passed into sort.Sort
 	sort.Sort(sort.Reverse(sort.StringSlice(s)))
+	// works to sort type StringSlice
+	//sort.Sort(sort.StringSlice(s))
 	fmt.Println(s)
 }

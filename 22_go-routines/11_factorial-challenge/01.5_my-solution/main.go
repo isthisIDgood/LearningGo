6aa4b6a3680c31642774e13b5d70ae6518c187f4
isthisIDgood <34673447+isthisIDgood@users.noreply.github.com>
2018-09-10 06:53:15 -0400

    changes factorial solution

--- 22_go-routines/11_factorial-challenge/01.5_my-solution/main.go
+++ 22_go-routines/11_factorial-challenge/01.5_my-solution/main.go
@@ -12,17 +12,17 @@ func main() {
 }
 
 func factorial(n int) chan int {
-	c := make(chan int)
+	out := make(chan int)
 	go func() {
 		total := 1
 		for i := n; i > 0; i-- {
 			total *= i
+			out <- total
 		}
-		c <- total
-		close(c)
+		close(out)
 	}()
-	return c
-	}
+	return out
+}
 //	go func() {
 //		total := 1
 //		for i := n; i > 0; i-- {
